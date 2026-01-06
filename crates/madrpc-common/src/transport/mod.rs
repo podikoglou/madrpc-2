152ac6d742c7774b4e5fbc8baea83b5b3d0473d1
Alex <alex.podikoglou@gmail.com>
2026-01-06 19:42:00 +0200

    refactor(transport): update codec export to JsonCodec

--- crates/madrpc-common/src/transport/mod.rs
+++ crates/madrpc-common/src/transport/mod.rs
@@ -2,9 +2,9 @@ pub mod codec;
 pub mod tcp;
 pub mod tcp_server;
 
-pub use codec::PostcardCodec;
+pub use codec::JsonCodec;
 pub use tcp::{TcpTransport, TcpTransportAsync};
-pub use tcp_server::{TcpServer, TcpServerThreaded};
+pub use tcp_server::TcpServer;
 
 #[cfg(test)]
 mod tests;
